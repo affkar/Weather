81c0052c354f3f940d8198e607f18a7e8a5c714c
Karthick Meenakshi Sundaram <affkar@gmail.com>
2015-03-06 12:04:24 +0000

    WeatherBundles

---
+++ WeatherBundles/TemperatureServices/main/java/com/jasmine/gethumidity/GetHumidityImpl.java
@@ -0,0 +1,10 @@
+package com.jasmine.gethumidity;
+
+public class GetHumidityImpl implements GetHumidity{
+
+	public String getHumidity(String in) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
